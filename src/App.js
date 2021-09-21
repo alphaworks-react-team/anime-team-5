6641ac822f9fab08bb7935c89cd22aafc40a01d5
jerniceduncan <jerniceduncan@icloud.com>
2021-09-21 16:08:46 -0700

    update

--- src/App.js
+++ src/App.js
@@ -19,13 +19,6 @@ const Img = styled.img`
   border-radius: 5px;
 `;
 
-const Card = styled.div`
-  width: 50%;
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  transition: 0.3s;
-  border-radius: 5px;
-`;
-
 const H2 = styled.h2`
 
 `
